84d172d8a0f704ead1e9f8165e1426afcb239f3d
mulf <mail@mulf.wtf>
2025-06-17 13:42:30 -0500

    remove rainbow kit connect btn

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -11,7 +11,6 @@ export function Navigation() {
                     </div>
                     <span className="text-xl font-bold text-white">MiniMart</span>
                 </div>
-                <ConnectButton />
             </nav>
         </header>
     );
