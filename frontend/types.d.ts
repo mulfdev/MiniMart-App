7aa2d13bd256f3fe2916fa68c81bb429670f9d8b
mulf <mail@mulf.wtf>
2025-06-18 18:44:13 -0500

    alchemy api update

--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -1,16 +1,30 @@
-export interface Nft {
-    collection: string;
-    contract: string;
-    description: string;
-    display_animation_url: string | null;
-    display_image_url: string;
-    identifier: string;
-    image_url: string;
-    is_disabled: boolean;
-    is_nsfw: boolean;
-    metadata_url: string;
+export interface Contract {
+    address: string;
     name: string;
-    opensea_url: string;
-    token_standard: string;
-    updated_at: string; // ISO 8601 datetime string
+    symbol: string;
+    totalSupply: number | null;
+    tokenType: string; // Consider making this a literal type if possible, e.g., "ERC721" | "ERC1155" etc.
+    contractDeployer: string;
+    deployedBlockNumber: number;
+    isSpam: boolean;
+    spamClassifications: string[]; // Assuming spamClassifications is an array of strings
+}
+
+export interface Image {
+    cachedUrl: string | null;
+    thumbnailUrl: string | null;
+    pngUrl: string | null;
+    contentType: string | null;
+    size: number | null;
+    originalUrl: string | null;
+}
+
+export interface Nft {
+    contract: Contract;
+    tokenId: string;
+    tokenType: string; // Consider making this a literal type if possible, e.g., "ERC721" | "ERC1155" etc.
+    name: string | null;
+    description: string | null;
+    tokenUri: string;
+    image: Image;
 }
