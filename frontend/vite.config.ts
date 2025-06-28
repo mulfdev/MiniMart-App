e447fd2a5c09ab2fe5285f5012232c368d0c7c35
mulf <mail@mulf.wtf>
2025-06-27 19:04:28 -0500

    vite workspace config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,13 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    server: {
+        fs: {
+            allow: [searchForWorkspaceRoot(process.cwd())],
+        },
+    },
 });
