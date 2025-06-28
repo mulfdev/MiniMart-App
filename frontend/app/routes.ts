272c70c8c3088b2e5f5f7071e6e567ddb579a68b
mulf <mail@mulf.wtf>
2025-06-27 19:04:19 -0500

    route config

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -4,4 +4,5 @@ export default [
     index('./routes/home.tsx'),
     route('/view/:address', './routes/view.tsx'),
     route('/list/:contract/:tokenId', './routes/list.tsx'),
+    route('/view/token/:contract/:tokenId', './routes/view/token.tsx'),
 ] satisfies RouteConfig;
