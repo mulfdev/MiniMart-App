531f1a218a02ed389282269198abc3046e263495
mulf <mail@mulf.wtf>
2025-06-27 19:03:22 -0500

    rename

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -7,7 +7,7 @@ const { TURSO_TOKEN, TURSO_DB_URL } = process.env;
 assert(typeof TURSO_TOKEN !== 'undefined', 'TURSO_TOKEN must be defined');
 assert(typeof TURSO_DB_URL !== 'undefined', 'TURSO_DB_URL must be defined');
 
-export const turso = createClient({
+export const db = createClient({
     url: TURSO_DB_URL,
     authToken: TURSO_TOKEN,
 });
