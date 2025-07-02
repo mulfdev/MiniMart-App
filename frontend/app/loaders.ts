c47a3618c7e9d6d18002c5ac4476a976213f8148
mulf <mail@mulf.wtf>
2025-07-02 16:19:13 -0500

    update data fetching logic

--- frontend/app/loaders.ts
+++ frontend/app/loaders.ts
@@ -1,25 +1,30 @@
-import type { Nft } from '@minimart/types';
-import { ALCHEMY_API_KEY } from '~/root';
-import type { Route as ListRoute } from './routes/+types/list';
-
+import type { Nft, OrderListed } from '@minimart/types';
+import { ALCHEMY_API_KEY, API_URL } from '~/root';
 export type ListNftLoaderData = Nft | null;
 
-export async function fetchNft(contract: string, tokenId: string): Promise<ListNftLoaderData> {
+interface TokenWithOrderData {
+    nft: Nft;
+    orderData: OrderListed;
+}
+
+export async function fetchNft(
+    contract: string,
+    tokenId: string
+): Promise<TokenWithOrderData | null> {
     if (!contract || !tokenId) {
         console.error('Contract address or token ID missing in params.');
         return null;
     }
+
     try {
-        const res = await fetch(
-            `https://base-sepolia.g.alchemy.com/nft/v3/${ALCHEMY_API_KEY}/getNFTMetadata?contractAddress=${contract}&tokenId=${tokenId}&tokenType=ERC721&refreshCache=false`
-        );
-        if (!res.ok) {
-            const errorText = await res.text();
-            console.error(`Error fetching NFT metadata: ${res.status} - ${errorText}`);
-            return null;
-        }
-        const data = await res.json();
-        return data && data.contract && data.tokenId ? (data as Nft) : null;
+        const url = new URL(`${API_URL}/single-token`);
+
+        url.searchParams.set('nftContract', contract);
+        url.searchParams.set('tokenId', tokenId);
+
+        const res = await fetch(url);
+        const data = (await res.json()) as TokenWithOrderData;
+        return data;
     } catch (error) {
         console.error('Failed to fetch NFT metadata:', error);
         return null;
