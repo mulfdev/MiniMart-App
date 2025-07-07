88828b09087953fc61c19a69b0ab824ac7404dc1
mulf <mail@mulf.wtf>
2025-07-07 15:19:01 -0500

    move cache into hook, add cache keys

--- frontend/app/hooks/useCache.ts
+++ frontend/app/hooks/useCache.ts
@@ -1,8 +1,46 @@
-import { get, set } from '~/cache';
+interface CacheEntry<T> {
+    value: T;
+    expiry: number;
+}
 
 const statusCache = new Map<string, { promise?: Promise<any>; error?: any }>();
 
-export function primeCache<T>(key: string, fetcher: () => Promise<T>, { ttl }: { ttl: number }) {
+const Cache = new Map<string, CacheEntry<unknown> | undefined>();
+
+export const cacheKeys = {
+    homepageOrders: 'homepageOrders',
+    nft: (contract: string, tokenId: string) => `nft:${contract}:${tokenId}`,
+    nfts: (address: string) => `nfts:${address}`,
+    listings: (address: string) => `listings:${address}`,
+} as const;
+
+export function get<T>(key: string) {
+    const entry = Cache.get(key);
+    if (typeof entry === 'undefined') return undefined;
+
+    if (entry.expiry < Date.now()) {
+        Cache.delete(key);
+        return undefined;
+    }
+    return entry.value as T;
+}
+
+export function set({ key, value, ttl }: { key: string; value: unknown; ttl: number }) {
+    Cache.set(key, {
+        value: value,
+        expiry: Date.now() + ttl,
+    });
+}
+
+export function remove(key: string) {
+    Cache.delete(key);
+}
+
+export function primeCache<T extends () => Promise<any>>(
+    key: string,
+    fetcher: T,
+    { ttl }: { ttl: number }
+) {
     if (get(key) !== undefined || statusCache.has(key)) {
         return;
     }
@@ -22,15 +60,15 @@ export function primeCache<T>(key: string, fetcher: () => Promise<T>, { ttl }: {
     statusCache.set(key, { promise });
 }
 
-export function useCache<T>(
+export function useCache<T extends () => Promise<any>>(
     key: string,
-    fetcher: () => Promise<T>,
-    { ttl, enabled = true }: { ttl: number; enabled: boolean }
-) {
+    fetcher: T,
+    { ttl, enabled = true }: { ttl: number; enabled?: boolean }
+): Awaited<ReturnType<T>> | undefined {
     if (!enabled) return;
-    const cachedData = get(key) as T | undefined;
+    const cachedData = get(key);
     if (cachedData !== undefined) {
-        return cachedData;
+        return cachedData as Awaited<ReturnType<T>>;
     }
 
     const status = statusCache.get(key);
@@ -58,3 +96,4 @@ export function useCache<T>(
     statusCache.set(key, { promise });
     throw promise;
 }
+
