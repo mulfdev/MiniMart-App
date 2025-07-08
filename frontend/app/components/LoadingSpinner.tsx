cbf17a9efffbbbfb21cbd096a2ebbaf01b6aac65
mulf <mail@mulf.wtf>
2025-07-08 18:08:28 -0500

    made loader size dynamic

--- frontend/app/components/LoadingSpinner.tsx
+++ frontend/app/components/LoadingSpinner.tsx
@@ -1,9 +1,13 @@
 
-export function LoadingSpinner() {
+export function LoadingSpinner({ className }: { className?: string }) {
+    const defaultStyles =
+        'w-6 h-6 border-2 border-blue-500/30 border-t-blue-500 rounded-full animate-spin';
     return (
         <div className="relative">
-            <div className="w-8 h-8 border-2 border-blue-500/30 border-t-blue-500 rounded-full animate-spin" />
-            <div className="absolute inset-0 w-8 h-8 border-2 border-purple-500/20 border-b-purple-500 rounded-full animate-spin animate-reverse" />
+            <div className={`${defaultStyles} ${className}`} />
+            <div
+                className={`${defaultStyles} ${className} absolute inset-0 border-purple-500/20 border-b-purple-500 animate-reverse`}
+            />
         </div>
     );
 }
