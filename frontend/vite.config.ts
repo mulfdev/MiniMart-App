4d7672b8bc9fb967e093d3d64c8f43c1038d36b5
mulf <mail@mulf.wtf>
2025-07-09 11:19:56 -0500

    check bundle size

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,18 @@ import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    plugins: [
+        tailwindcss(),
+        reactRouter(),
+        tsconfigPaths(),
+        visualizer({
+            open: true,
+            filename: 'bundle-stats.html',
+        }),
+    ],
     server: {
         fs: {
             allow: [searchForWorkspaceRoot(process.cwd())],
