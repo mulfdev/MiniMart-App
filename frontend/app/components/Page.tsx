0150659eb3c41c33e4ffcee8af680407c8f0576b
mulf <mail@mulf.wtf>
2025-07-09 20:20:52 -0500

    tweak pading and update return type for new json response

--- frontend/app/components/Page.tsx
+++ frontend/app/components/Page.tsx
@@ -1,4 +1,4 @@
-import type { Nft } from '@minimart/types';
+import type { Nft, OrderListed } from '@minimart/types';
 import { NftCard } from './NftCard';
 
 export function Page({
@@ -12,7 +12,7 @@ export function Page({
 }) {
     return (
         <div className="max-w-7xl mx-auto">
-            <main className="container mx-auto px-4 py-8 sm:py-16">
+            <main className="container mx-auto px-4 py-8">
                 <div className="text-center mb-12 sm:mb-16">
                     <h1 className="text-3xl sm:text-4xl font-bold text-white mb-4">{title}</h1>
                     <p className="text-lg text-zinc-400 max-w-2xl mx-auto">{description}</p>
@@ -23,10 +23,16 @@ export function Page({
     );
 }
 
-export function NftGrid({ nfts, variant }: { nfts: Nft[]; variant?: 'view' | 'remove' }) {
+export function NftGrid({
+    nfts,
+    variant,
+}: {
+    nfts: { nft: Nft; orderInfo: OrderListed }[];
+    variant?: 'view' | 'remove';
+}) {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 mb-22">
-            {nfts.map((nft, index) => (
+            {nfts.map(({ nft }, index) => (
                 <div
                     key={`${nft.contract.address}+${nft.tokenId}`}
                     className="animate-in fade-in slide-in-from-bottom-4"
