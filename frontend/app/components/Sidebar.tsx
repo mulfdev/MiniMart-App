246282846b262d8908108c294db2cb32b616e717
mulf <mail@mulf.wtf>
2025-07-11 12:45:10 -0500

    cleanup and more bottom padding

--- frontend/app/components/Sidebar.tsx
+++ frontend/app/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, type JSX, useRef } from 'react';
+import { lazy, type JSX, useRef } from 'react';
 import { Home, Shapes, NotebookTabs, Logs } from 'lucide-react';
 import { Link } from 'react-router';
 import { useAccount, useConnect } from 'wagmi';
@@ -10,7 +10,6 @@ export function Sidebar({ isOpen, onClose }: { isOpen: boolean; onClose: () => v
     const ref = useRef<HTMLDivElement>(null);
     const { address } = useAccount();
     const { connect, connectors } = useConnect();
-
     useOnClickOutside(ref, onClose);
 
     function SidebarLink({ url, label, icon }: { url: string; label: string; icon: JSX.Element }) {
@@ -30,7 +29,7 @@ export function Sidebar({ isOpen, onClose }: { isOpen: boolean; onClose: () => v
             ref={ref}
             className={`fixed top-0 right-0 bg-zinc-900/80 backdrop-blur-sm z-50 rounded-lg transition-transform duration-100 ease-in-out h-[100svh] ${
                 isOpen ? 'translate-x-0 visibility-visible' : 'translate-x-full visibility-hidden'
-            } w-64 p-4`}
+            } w-64 p-6 pb-8`}
         >
             <div className=" pt-2 flex flex-col gap-y-4 h-full">
                 <SidebarLink
