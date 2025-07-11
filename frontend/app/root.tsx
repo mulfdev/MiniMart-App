cbb729fb1739d6a0ebef753f8375a4234fcc2bd3
mulf <mail@mulf.wtf>
2025-07-11 18:53:34 -0500

    make the grid more visible

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -59,7 +59,7 @@ const backgroundStyle = {
             radial-gradient(ellipse 80% 50% at 50% -20%, hsla(220, 100%, 50%, 0.05), transparent),
             radial-gradient(ellipse 60% 80% at 80% 50%, hsla(280, 100%, 50%, 0.03), transparent),
             radial-gradient(ellipse 40% 60% at 20% 80%, hsla(300, 100%, 50%, 0.02), transparent),
-            url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="rgb(255,255,255,0.02)"%3E%3Cpath d="M0 .5H31.5V32"/%3E%3C/svg%3E')
+            url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="rgb(255,255,255,0.05)"%3E%3Cpath d="M0 .5H31.5V32"/%3E%3C/svg%3E')
         `,
 };
 
