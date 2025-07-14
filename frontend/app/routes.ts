6a8ff3b82528a392b55aa8933cdc6c9234370941
mulf <mail@mulf.wtf>
2025-07-14 12:29:37 -0500

    add new reorg rotues

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -2,10 +2,10 @@ import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
 export default [
     index('./routes/home.tsx'),
-    route('/user/:address', './routes/view.tsx'),
+    route('/user/:address', './routes/user/view.tsx'),
     route('/user/listings/:address', './routes/user/listings.tsx'),
     route('/user/listings/:address/completed', './routes/user/completedOrders.tsx'),
     route('/list/:contract/:tokenId', './routes/list.tsx'),
-    route('/token/:contract/:tokenId', './routes/user/token.tsx'),
+    route('/token/:contract/:tokenId', './routes/token.tsx'),
     route('orders', './routes/allOrders.tsx'),
 ] satisfies RouteConfig;
