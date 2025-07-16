4540628e1049b3798227996967d51a2e493196c7
mulf <mail@mulf.wtf>
2025-07-16 15:49:54 -0500

    mainnet

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,17 +1,17 @@
 import { createConfig, http } from 'wagmi';
-import { baseSepolia } from 'wagmi/chains';
+import { base } from 'wagmi/chains';
 import { farcasterFrame as miniAppConnector } from '@farcaster/frame-wagmi-connector';
 
-export const BASE_SEPOLIA_RPC_URL = import.meta.env.VITE_BASE_SEPOLIA_RPC_URL;
+export const BASE_RPC_URL = import.meta.env.VITE_BASE_RPC_URL;
 
-if (typeof BASE_SEPOLIA_RPC_URL !== 'string') {
+if (typeof BASE_RPC_URL !== 'string') {
     throw new Error('BASE_RPC_URL must be set');
 }
 
 export const wagmiConfig = createConfig({
-    chains: [baseSepolia],
+    chains: [base],
     transports: {
-        [baseSepolia.id]: http(BASE_SEPOLIA_RPC_URL),
+        [base.id]: http(BASE_RPC_URL),
     },
     connectors: [miniAppConnector()],
 });
