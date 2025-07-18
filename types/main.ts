e75bda5d6d3865e9d71e3e8c3ed4e07b167bebab
mulf <mail@mulf.wtf>
2025-07-18 14:34:56 -0500

    new types

--- types/main.ts
+++ types/main.ts
@@ -30,18 +30,73 @@ export interface Nft {
     orderId?: string;
 }
 
-export interface OrderListed {
-    id: string;
-    orderId: string;
-    seller: string;
-    nftContract: string;
-    tokenId: string;
-    price: string;
-    blockNumber: number;
-    blockTimestamp: number;
-    transactionHash: string;
-}
-
 export interface GetOrderListedEvents {
     orderListeds: OrderListed[];
 }
+
+interface EventBase {
+    blockNumber: bigint;
+    transactionHash: string;
+}
+
+export interface EIP712DomainChanged extends EventBase {
+    eventName: 'EIP712DomainChanged';
+    args: null;
+}
+
+export interface OrderFulfilled extends EventBase {
+    eventName: 'OrderFulfilled';
+    args: {
+        orderId: string;
+        buyer: string;
+    };
+}
+
+export interface OrderListed extends EventBase {
+    eventName: 'OrderListed';
+    args: {
+        orderId: string;
+        seller: string;
+        nftContract: string;
+        tokenId: bigint;
+        price: bigint;
+    };
+}
+
+export interface OrderRemoved extends EventBase {
+    eventName: 'OrderRemoved';
+    args: {
+        orderId: string;
+    };
+}
+
+export interface OwnershipTransferred extends EventBase {
+    eventName: 'OwnershipTransferred';
+    args: {
+        previousOwner: string;
+        newOwner: string;
+    };
+}
+
+export interface Paused extends EventBase {
+    eventName: 'Paused';
+    args: {
+        account: string;
+    };
+}
+
+export interface Unpaused extends EventBase {
+    eventName: 'Unpaused';
+    args: {
+        account: string;
+    };
+}
+
+export type MiniMartEvents =
+    | EIP712DomainChanged
+    | OrderFulfilled
+    | OrderListed
+    | OrderRemoved
+    | OwnershipTransferred
+    | Paused
+    | Unpaused;
